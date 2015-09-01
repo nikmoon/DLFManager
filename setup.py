d4bb9647fbc3735971e114f06df6dec78a34b157
unknown <consensuss@rambler.ru>
2015-09-01 13:04:17 +0700

    Project structure changed for setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name = "TorrentManager",
+	version = "0.1",
+	packages = ['TorrentManager'],
+	package_data = {'TorrentManager': ['qt-logo.png', 'MainWindow.ui', 'LICENSE', 'README.md']},
+)
