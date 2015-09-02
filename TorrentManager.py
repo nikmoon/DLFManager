7cc0ac32526b19f15e862a6359b4242eebe6f5b0
unknown <consensuss@rambler.ru>
2015-09-02 17:30:47 +0700

    refactoring

--- TorrentManager.py
+++ TorrentManager.py
@@ -1,89 +1,105 @@
+#
 # -*- coding: utf-8 -*-
+#
 
 import errno
 import sys
 import os
-import types
-from DLFManager import MyLib
+
+from DLFManager.ConfigFile import MyLib, ConfigFile, APP_DIR, CFG_FILE_BASE_NAME, CFG_FILE_NAME
+
 from PyQt4 import QtCore, QtGui, uic
-from PyQt4.QtGui import QListWidget, QWidget, QHBoxLayout, QVBoxLayout, QSplitter, QColor
+from PyQt4.QtGui import (
+	QListWidget, QWidget, QHBoxLayout, QVBoxLayout, QSplitter, QColor, QListWidgetItem,
+	QMessageBox, QIcon, QMainWindow
+)
+from PyQt4.QtCore import Qt
 
-from DLFManager.ConfigFile import ConfigFile
-from DLFManager import APP_DIR, CFG_FILE_BASE_NAME, CFG_FILE_NAME
 
 
-"""
------------------------------------------------------
-Алгоритм работы программы
------------------------------------------------------
-	1.	Инициализация.
-	2.	Чтение конфигурационного файла.
-	3.	Управление списком рабочих каталогов:
-		а. добавление нового каталога в список рабочих
-		б. удаление существующего каталога из списка рабочих
-		в. выбор каталога для работы
-	4.	Управление рабочим каталогом.
-	5.	Сохранение несохраненных изменений.
-	6.	Выход.
-"""
+app = QtGui.QApplication(sys.argv)
+MAIN_WINDOW_UI_FILE = os.path.join(APP_DIR, u"MainWindow.ui")
+MAIN_WINDOW_ICON = QIcon(os.path.join(APP_DIR, u"qt-logo.png"))
 
+ASK_SAVE_CFG_ON_EXIT = False
+
+
+class MyListWidgetItem(QListWidgetItem):
+	def __init__(self, text):
+		QListWidgetItem.__init__(self, text)
+		self.setTextColor(QColor(0, 0, 255))
 
 
 class MainWindow(QtGui.QMainWindow):
 
-	def __init__(self, qApp):
-		self.qApp = qApp
+	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
-		uic.loadUi(os.path.join(APP_DIR, u"MainWindow.ui"), self)
-		self.setWindowIcon()
+		uic.loadUi(MAIN_WINDOW_UI_FILE, self)
+		self.setWindowIcon(MAIN_WINDOW_ICON)
 		self.configFile = ConfigFile()
 		self.workingDirs = self.configFile.read()
+		self.walkDir = APP_DIR
 
 		self.action.triggered.connect(self.configFile.backup)
 		self.action_2.triggered.connect(self.configFile.saveDefault)
+		self.action_3.triggered.connect(self.close)
 
-		self.walkDir = APP_DIR
+		# сохраняем дефолные обработчики
+		self.lwMain.defKeyPressEvent = self.lwMain.keyPressEvent
+		self.lwAux.defKeyPressEvent = self.lwAux.keyPressEvent
+
+		# связываем QLineEdit с соответствующими QListWidget
+		self.lwMain.bindedLineEdit = self.leMain
+		self.lwAux.bindedLineEdit = self.leAux
 
 		self.startManageWorkingDirs()
 
-	# переходим к управлению списком рабочих каталогов
+
+	# режим управления списком рабочих каталогов
 	def startManageWorkingDirs(self):
-		self.leMain.setText(u"Управление списком рабочих каталогов")
+		self.leMain.setText(u"Список рабочих каталогов:")
 		self.lwMain.clear()
-
-		self.lwMain.currentRowChanged.connect(self.showDirContent)
-		self.lwMain.defKeyPressEvent = self.lwMain.keyPressEvent
-		self.lwMain.keyPressEvent = self.keyPressedOnManageWorkingDirsList
-
+		self.lwMain.currentRowChanged.connect(self.onChangeCurrentWorkingDir)
+		self.lwMain.keyPressEvent = self.onKeyPress_lwMain_manageWorkingDirs
 		self.lwMain.setFocus()
-
-		for wkDirName in self.workingDirs:
-			item = QtGui.QListWidgetItem(wkDirName)
+		for wkDirName in sorted(self.workingDirs):
+			item = MyListWidgetItem(wkDirName)
 			if not self.workingDirs[wkDirName]["exists"]:
 				item.setTextColor(QColor(255, 0, 0))
 			self.lwMain.addItem(item)
 		self.lwMain.setCurrentRow(0)
 
 
-	def showDirContent(self, rowNum):
-		wkDirName = unicode(self.lwMain.item(rowNum).text())
-		self.leAux.setText(wkDirName)
-		self.lwAux.clear()
-		self.fillListWidgetWithDirContent(wkDirName, self.lwAux)
+	def onChangeCurrentWorkingDir(self, rowNum):
+		self.showWorkingDirContent(unicode(self.lwMain.item(rowNum).text()), self.lwAux)
 
 
-	def fillListWidgetWithDirContent(self, dirName, lWidget):
-		for entry in self.workingDirs[dirName]["new"]:	# неучтенные элементы в рабочем каталоге
-			item = QtGui.QListWidgetItem(entry)
-			item.setTextColor(QColor(0, 150, 0))
-			lWidget.addItem(item)
-		for entry in self.workingDirs[dirName]["entries"]:	# учтенные элементы
-			item = QtGui.QListWidgetItem(entry)
-			if not self.workingDirs[dirName]["entries"][entry]["exists"]:		# учтенный элемент уже не существует
-				item.setTextColor(QColor(255, 0, 0))
-			lWidget.addItem(item)
-		
+	def showWorkingDirContent(self, wkDirName, lw):
+		lw.bindedLineEdit.setText(wkDirName)
+		lw.clear()
+		wkDir = self.workingDirs[wkDirName]
+		entryList = sorted(wkDir["new"] + wkDir["entries"].keys())
+		for entry in entryList:
+			item = MyListWidgetItem(entry)
+			if entry in wkDir["entries"]:
+				if not wkDir["entries"][entry]["exists"]:
+					item.setTextColor(QColor(255, 0, 0))
+			else:
+				item.setTextColor(QColor(0, 150, 0))
+			lw.addItem(item)
 
+
+	def onKeyPress_lwMain_manageWorkingDirs(self, keyEvent):
+		if keyEvent.key() == QtCore.Qt.Key_Return:		# переход в режим управления выбранным рабочим каталогом
+			print u"Переходим в режим управления выбранным рабочим каталогом"
+		elif keyEvent.key() == QtCore.Qt.Key_Insert:	# переходим в режим добавления нового рабочего каталога
+			print u"Переходим в режим добавления нового рабочего каталога"
+		elif keyEvent.key() == QtCore.Qt.Key_Delete:	# удаление каталога из списка рабочих
+			print u"Удаление каталога из списка рабочих"
+		self.lwMain.defKeyPressEvent(keyEvent)
+
+
+	'''
 	# Обработка нажатий клавиш в главном QListWidget в режиме управления рабочими каталогами
 	def keyPressedOnManageWorkingDirsList(self, keyEvent):
 		if keyEvent.key() == QtCore.Qt.Key_Escape:
@@ -92,7 +108,7 @@ class MainWindow(QtGui.QMainWindow):
 			self.currentWkDir = unicode(self.lwMain.currentItem().text())
 			if self.workingDirs[self.currentWkDir]["exists"]:
 				QListWidget.keyPressEvent(self.lwMain, keyEvent)
-				self.lwMain.currentRowChanged.disconnect(self.showDirContent)
+				#self.lwMain.currentRowChanged.disconnect(self.showDirContent)
 				self.lwMain.keyPressEvent = self.lwMain.defKeyPressEvent
 				self.startManageSelectedWkDir()
 				return
@@ -102,10 +118,10 @@ class MainWindow(QtGui.QMainWindow):
 		else:
 			pass
 		QListWidget.keyPressEvent(self.lwMain, keyEvent)
+	'''
 
 
 	def startAddingNewWorkingDir(self):
-
 		self.lwMain.currentRowChanged.disconnect(self.showDirContent)
 		self.lwMain.keyPressEvent = self.keyPressedOnAddingNewWorkingDir
 		self.lwMain.clear()
@@ -152,21 +168,17 @@ class MainWindow(QtGui.QMainWindow):
 	def startManageSelectedWkDir(self):
 		self.leMain.setText(self.currentWkDir)
 		self.lwMain.clear()
-
 		self.lwMain.currentRowChanged.connect(self.showLinksForEntry)
 		self.lwMain.keyPressEvent = self.keyPressedOnManageSelectedWkDir
-
 		self.fillListWidgetWithDirContent(self.currentWkDir, self.lwMain)
 		self.lwMain.setCurrentRow(0)
 
-	
+	# обработка нажатия клавиш в режиме управления рабочим каталогом
 	def keyPressedOnManageSelectedWkDir(self, keyEvent):
 		if keyEvent.key() == QtCore.Qt.Key_Escape:		# возвращаемся к управлению рабочими каталогами
-			QListWidget.keyPressEvent(self.lwMain, keyEvent)
 			self.lwMain.currentRowChanged.disconnect(self.showLinksForEntry)
 			self.lwMain.keyPressEvent = self.lwMain.defKeyPressEvent
 			self.startManageWorkingDirs()
-			return
 		QListWidget.keyPressEvent(self.lwMain, keyEvent)
 
 
@@ -174,29 +186,33 @@ class MainWindow(QtGui.QMainWindow):
 		entryName = unicode(self.lwMain.item(rowNum).text())
 		self.leAux.setText(u"Ссылки для {0}".format(entryName))
 		self.lwAux.clear()
-
 		if entryName in self.workingDirs[self.currentWkDir]["entries"]:
 			for link in self.workingDirs[self.currentWkDir]["entries"][entryName]["links"]:
 				item = QtGui.QListWidgetItem(link)
 				self.lwAux.addItem(item)
 			self.lwAux.addItem(QtGui.QListWidgetItem(u"Здесь должны быть ссылки"))
 
-	def setWindowIcon(self):
-		myIcon = QtGui.QIcon(os.path.join(APP_DIR, u"qt-logo.png"))
-		QtGui.QMainWindow.setWindowIcon(self, myIcon)		
 
-	"""
-	пример использования палитры
-	def initialize(self):
-		palette = self.lwAux.palette()
-		palette.setColor(QtGui.QPalette.Text, QColor(0,255,0))
-		self.lwAux.setPalette(palette)
-	"""
+	def closeEvent(self, event):
+		msgBox = QMessageBox(text = u"Действительно закрыть приложение?", parent = self)
+		msgBox.setWindowTitle(u"Подтверждение")
+		msgBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+		msgBox.setDefaultButton(QMessageBox.Yes)
+		if msgBox.exec_() == QMessageBox.No:
+			event.ignore()
+		elif ASK_SAVE_CFG_ON_EXIT:
+			msgBox.setText(u"Сохранить файл конфигурации?")
+			msgBox.setDefaultButton(QMessageBox.Yes)
+			if msgBox.exec_() == QMessageBox.Yes:
+				self.configFile.save()
+			event.accept()
+		else:
+			event.accept()
+
 
 		
 def main():
-	app = QtGui.QApplication(sys.argv)
-	mainWindow = MainWindow(app)
+	mainWindow = MainWindow()
 	mainWindow.show()
 	sys.exit(app.exec_())
 
